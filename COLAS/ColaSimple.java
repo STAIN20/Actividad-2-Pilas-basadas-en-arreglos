e869b81376a50c296a4c577e9098d9a6a68b30c8
Luis Fernando Reyes Betancourt <128568936+STAIN20@users.noreply.github.com>
2023-09-17 16:07:16 -0600

    Add files via upload

---
+++ COLAS/ColaSimple.java
@@ -0,0 +1,45 @@
+//Luis Fernando Reyes Betancourt
+public class ColaSimple {
+
+	private Nodo frente;
+	private int longitud;
+
+	public ColaSimple() {
+		frente = null;
+		longitud = 0;
+	}
+
+	public void insertar(int dato) {
+		Nodo nuevoNodo = new Nodo(dato);
+		if (frente == null) {
+			frente = nuevoNodo;
+		} else {
+			Nodo nodoActual = frente;
+			while (nodoActual.getSiguiente() != null) {
+				nodoActual = nodoActual.getSiguiente();
+			}
+			nodoActual.setSiguiente(nuevoNodo);
+		}
+		longitud++;
+	}
+
+	public void eliminar() {
+		if (frente != null) {
+			frente = frente.getSiguiente();
+			longitud--;
+		}
+	}
+
+	public boolean estaVacia() {
+		return frente == null;
+	}
+
+	public Nodo getFrente() {
+		return frente;
+	}
+
+	public int getLongitud() {
+		return longitud;
+	}
+
+}
