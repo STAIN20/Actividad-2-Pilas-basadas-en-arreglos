b4ffb40f0b74832ff08fd0c45b24c83738fb65a8
Luis Fernando Reyes Betancourt <luisfrb456@gmail.com>
2023-11-13 17:53:45 -0600

    Mejoras de codigo sin comentar

--- Arboles/Arbol.java
+++ Arboles/Arbol.java
@@ -2,73 +2,110 @@
  * Clase principal Arbol
  */
 public class Arbol <T> {                    
-    private NodoArbol <T> raiz;             
-    private boolean esBinario = false;      
-  
-    /**
-     * Constructor para inicializar el nodo raiz con el dato proporcionado y checa si es un arbol binario o no
-     * @param dato
-     * @param esBinario                         
-     */
-    public Arbol (T dato, boolean esBinario) {
-      raiz = new NodoArbol <T> (dato);             
-      raiz.setEsBinario(esBinario);                 
-      this.esBinario = esBinario;
-    }
-  
-    /**
-     * Método que nos retornará el nodo raíz
-     * @return
-     */
-    public NodoArbol <T> obtenerRaiz() {
-      return raiz;                           
-    }
-  
-    /**
-     * Este booleano nos verifica y retorna un "True" o "False" en caso de ser o no un arbol binario
-     * @return
-     */
-    public boolean esBinario() {            
-      return esBinario;                     
-    }
-  
-    /**
-     * Este metodo agrega un nodo (hijo) al arbol y se le agrega la propiedad de esBinario y lo toma como nodo hijo
-     * @param nodoRaiz
-     * @param nodoAgregado
-     */
-    public void agregarNodoArbol(NodoArbol <T> nodoRaiz, NodoArbol <T> nodoAgregado) {
-      if (esBinario) {
-        nodoAgregado.setEsBinario(esBinario);                  
-      }                                                         
-  
-      nodoRaiz.agregarHijo(nodoAgregado);
-    }
-  
-    /**
-     * Este metodo imprime el arbol de 3 diferentes ordenes de recorrido dependiendo del valor de "recorrido"
-     * Ordenes: Prefijo, infijo y posfijo
-     * @param recorrido
-     */
-    public void imprimirArbol(Recorrido recorrido) {
-      switch (recorrido.ordinal()) {
-        case 0:
-          System.out.println("Prefijo: ");         
-          raiz.imprimirEnPrefijo();                      
-          break;                                          
-  
-        case 1:
-          System.out.println("\nInfijo: ");
-          raiz.imprimirEnInfijo();
-          break;
-  
-        case 2:
-          System.out.println("\nPosfijo: ");
-          raiz.imprimirEnPosfijo();
-          break;
-      
-        default:
-          break;
-      }
+  private NodoArbol <T> raiz;             
+  private boolean esBinario = false;    
+  boolean estaVacio = false;  
+
+  /**
+   * Constructor para inicializar el nodo raiz con el dato proporcionado y checa si es un arbol binario o no
+   * @param dato
+   * @param esBinario                         
+   */
+  public Arbol (T dato, boolean esBinario) {
+    raiz = new NodoArbol <T> (dato);             
+    raiz.setEsBinario(esBinario);                 
+    this.esBinario = esBinario;
+  }
+
+  /**
+   * Método que nos retornará el nodo raíz
+   * @return
+   */
+  public NodoArbol <T> obtenerRaiz() {
+    return raiz;                           
+  }
+
+  /**
+   * Este booleano nos verifica y retorna un "True" o "False" en caso de ser o no un arbol binario
+   * @return
+   */
+  public boolean esBinario() {            
+    return esBinario;                     
+  }
+
+  /**
+   * Este metodo agrega un nodo (hijo) al arbol y se le agrega la propiedad de esBinario y lo toma como nodo hijo
+   * @param nodoRaiz
+   * @param nodoAgregado
+   */
+  public void agregarNodoArbol(NodoArbol <T> nodoRaiz, NodoArbol <T> nodoAgregado) {
+    if (esBinario) {
+      nodoAgregado.setEsBinario(esBinario);                  
+    }                                                         
+
+    nodoRaiz.agregarHijo(nodoAgregado);
+  }
+
+  /**
+   * Este metodo imprime el arbol de 3 diferentes ordenes de recorrido dependiendo del valor de "recorrido"
+   * Ordenes: Prefijo, infijo y posfijo
+   * @param recorrido
+   */
+  public void imprimirArbol(Recorrido recorrido) {
+    switch (recorrido.ordinal()) {
+      case 0:
+        System.out.println("Prefijo: ");         
+        raiz.imprimirEnPrefijo();                      
+        break;                                          
+
+      case 1:
+        System.out.println("\nInfijo: ");
+        raiz.imprimirEnInfijo();
+        break;
+
+      case 2:
+        System.out.println("\nPosfijo: ");
+        raiz.imprimirEnPosfijo();
+        break;
+    
+      default:
+        break;
     }
   }
+
+  public void imprimirSubArbol(NodoArbol<T> nodoSubArbol) {
+    System.out.println("Impresión en Prefijo: ");
+    nodoSubArbol.imprimirEnPrefijo();
+    System.out.println();
+
+    System.out.println("Impresión en Infijo: ");
+    nodoSubArbol.imprimirEnInfijo();
+    System.out.println();
+
+    System.out.println("Impresión en Posfijo");
+    nodoSubArbol.imprimirEnPosfijo();
+
+  }
+  
+  public boolean estaVacio() {
+    return estaVacio;
+  }
+
+  public int obtenerAltura() {
+    return obtenerAltura(raiz);
+  }
+
+  private int obtenerAltura(NodoArbol<T> nodo) {
+    if (nodo == null) {
+      return 0;
+    } else {
+      int altura = 0;
+      NodoArbol<T> hijo = nodo.obtenerPrimerHijo();
+      while (hijo != null) {
+        altura = Math.max(altura, obtenerAltura(hijo));
+        hijo = hijo.obtenerSiguienteHermano();
+      }
+      return altura + 1;
+    }
+  }
+}
--- Arboles/Main.java
+++ Arboles/Main.java
@@ -1,39 +1,127 @@
 public class Main {
-    public static void main(String[] args) {
-      Arbol<String> arbol = new Arbol<>("A", false);
+  public static void main(String[] args) {
+    Arbol<String> arbol = new Arbol<>("A", false);
   
-      NodoArbol<String> nodoB = new NodoArbol<>("B");
-      arbol.agregarNodoArbol(arbol.obtenerRaiz(), nodoB);
-      NodoArbol<String> nodoE = new NodoArbol<>("E");
-      arbol.agregarNodoArbol(nodoB, nodoE);
-  
-      NodoArbol<String> nodoF = new NodoArbol<>("F");
-      arbol.agregarNodoArbol(arbol.obtenerRaiz(), nodoF);
-      arbol.agregarNodoArbol(nodoF, new NodoArbol<>("J"));
-      arbol.agregarNodoArbol(nodoF, new NodoArbol<>("K"));
-      arbol.agregarNodoArbol(nodoF, new NodoArbol<>("L"));
-  
-      arbol.agregarNodoArbol(arbol.obtenerRaiz(), new NodoArbol<>("C"));
-  
-      NodoArbol<String> nodoD = new NodoArbol<>("D");
-      arbol.agregarNodoArbol(arbol.obtenerRaiz(), nodoD);
-      NodoArbol<String> nodoG = new NodoArbol<>("G");
-      arbol.agregarNodoArbol(nodoD, nodoG);
-      NodoArbol<String> nodoM = new NodoArbol<>("M");
-      arbol.agregarNodoArbol(nodoG, nodoM);
-      arbol.agregarNodoArbol(nodoM, new NodoArbol<>("P"));
-      arbol.agregarNodoArbol(nodoM, new NodoArbol<>("Q"));
-  
-      arbol.agregarNodoArbol(arbol.obtenerRaiz(), new NodoArbol<>("H"));
-  
-      NodoArbol<String> nodoI = new NodoArbol<>("I");
-      arbol.agregarNodoArbol(arbol.obtenerRaiz(), nodoI);
-      arbol.agregarNodoArbol(nodoI, new NodoArbol<>("N"));
-      arbol.agregarNodoArbol(nodoI, new NodoArbol<>("O"));
-  
-      arbol.imprimirArbol(Recorrido.PREFIJO);
-      arbol.imprimirArbol(Recorrido.INFIJO);
-      arbol.imprimirArbol(Recorrido.POSFIJO);
-    }
+    NodoArbol<String> nodoRaiz = arbol.obtenerRaiz();
+    NodoArbol<String> nodoB = new NodoArbol<>("B");
+    NodoArbol<String> nodoC = new NodoArbol<>("C");
+    NodoArbol<String> nodoD = new NodoArbol<>("D");
+    NodoArbol<String> nodoE = new NodoArbol<>("E");
+    NodoArbol<String> nodoF = new NodoArbol<>("F");
+    NodoArbol<String> nodoG = new NodoArbol<>("G");
+    NodoArbol<String> nodoH = new NodoArbol<>("H");
+    NodoArbol<String> nodoI = new NodoArbol<>("I");
+    NodoArbol<String> nodoJ = new NodoArbol<>("J");
+    NodoArbol<String> nodoK = new NodoArbol<>("K");
+    NodoArbol<String> nodoL = new NodoArbol<>("L");
+    NodoArbol<String> nodoM = new NodoArbol<>("M");
+    NodoArbol<String> nodoN = new NodoArbol<>("N");
+    NodoArbol<String> nodoO = new NodoArbol<>("O");
+    NodoArbol<String> nodoP = new NodoArbol<>("P");
+    NodoArbol<String> nodoQ = new NodoArbol<>("Q");
+
+    // Nodo A y su descendencia
+    arbol.agregarNodoArbol(nodoRaiz, nodoB);
+    arbol.agregarNodoArbol(nodoRaiz, nodoC);
+    arbol.agregarNodoArbol(nodoRaiz, nodoD);
+    // Nodo B y su descendencia
+    arbol.agregarNodoArbol(nodoB, nodoE);
+    arbol.agregarNodoArbol(nodoB, nodoF);
+    // Nodo F y su descendencia
+    arbol.agregarNodoArbol(nodoF, nodoJ);
+    arbol.agregarNodoArbol(nodoF, nodoK);
+    arbol.agregarNodoArbol(nodoF, nodoL);
+    // Nodo D y su descendencia
+    arbol.agregarNodoArbol(nodoD, nodoG);
+    arbol.agregarNodoArbol(nodoD, nodoH);
+    arbol.agregarNodoArbol(nodoD, nodoI);
+    // Nodo G y su descendencia
+    arbol.agregarNodoArbol(nodoG, nodoM);
+    // Nodo I y su descendencia
+    arbol.agregarNodoArbol(nodoI, nodoN);
+    arbol.agregarNodoArbol(nodoI, nodoO);
+    // Nodo M y su descendencia
+    arbol.agregarNodoArbol(nodoM, nodoP);
+    arbol.agregarNodoArbol(nodoM, nodoQ);
+
+  System.out.println("\n--Árbol Ordinario--");
+    
+  System.out.println();
+    arbol.imprimirArbol(Recorrido.PREFIJO);
+    arbol.imprimirArbol(Recorrido.INFIJO);
+    arbol.imprimirArbol(Recorrido.POSFIJO);
+
+    System.out.println();
+    System.out.println("\n--Altura del árbol: " + arbol.obtenerAltura());
+
+        System.out.println("\n--Subarbol B ");
+        arbol.imprimirSubArbol(nodoB);
+        System.out.println();
+
+        System.out.println("\n--¿El árbol está vacío?");
+          System.out.println(arbol.estaVacio() ? "Sí\n" : "No\n");
+
+        String raiz = nodoRaiz.getDato();
+        System.out.println("--Raíz del árbol: " + raiz);
+
+         System.out.println("\n--Árbol Binario--");
+
+        Arbol<String> arbolBin = new Arbol<String>("A", true);
+
+        NodoArbol<String> raizBin = arbolBin.obtenerRaiz();
+        NodoArbol<String> nodoBinB = new NodoArbol<>("B");
+        NodoArbol<String> nodoBinC = new NodoArbol<>("C");
+        NodoArbol<String> nodoBinD = new NodoArbol<>("D");
+        NodoArbol<String> nodoBinE = new NodoArbol<>("E");
+        NodoArbol<String> nodoBinF = new NodoArbol<>("F");
+        NodoArbol<String> nodoBinG = new NodoArbol<>("G");
+        NodoArbol<String> nodoBinH = new NodoArbol<>("H");
+        NodoArbol<String> nodoBinI = new NodoArbol<>("I");
+        NodoArbol<String> nodoBinJ = new NodoArbol<>("J");
+        NodoArbol<String> nodoBinK = new NodoArbol<>("K");
+        NodoArbol<String> nodoBinL = new NodoArbol<>("L");
+        NodoArbol<String> nodoBinM = new NodoArbol<>("M");
+        NodoArbol<String> nodoBinN = new NodoArbol<>("N");
+        NodoArbol<String> nodoBinO = new NodoArbol<>("O");
+        NodoArbol<String> nodoBinP = new NodoArbol<>("P");
+        NodoArbol<String> nodoBinQ = new NodoArbol<>("Q");
+
+        arbolBin.agregarNodoArbol(raizBin, nodoBinB);
+        arbolBin.agregarNodoArbol(nodoBinB, nodoBinE);
+        arbolBin.agregarNodoArbol(nodoBinB, nodoBinC);
+        arbolBin.agregarNodoArbol(nodoBinE, nodoBinF);
+        arbolBin.agregarNodoArbol(nodoBinF, nodoBinJ);
+        arbolBin.agregarNodoArbol(nodoBinJ, nodoBinK);
+        arbolBin.agregarNodoArbol(nodoBinK, nodoBinL);
+        arbolBin.agregarNodoArbol(nodoBinC, nodoBinD);
+        arbolBin.agregarNodoArbol(nodoBinD, nodoBinG);
+        arbolBin.agregarNodoArbol(nodoBinG, nodoBinM);
+        arbolBin.agregarNodoArbol(nodoBinG, nodoBinH);
+        arbolBin.agregarNodoArbol(nodoBinM, nodoBinP);
+        arbolBin.agregarNodoArbol(nodoBinP, nodoBinQ);
+        arbolBin.agregarNodoArbol(nodoBinH, nodoBinI);
+        arbolBin.agregarNodoArbol(nodoBinI, nodoBinN);
+        arbolBin.agregarNodoArbol(nodoBinN, nodoBinO);
+
+        System.out.println();
+        arbolBin.imprimirArbol(Recorrido.PREFIJO);
+        arbolBin.imprimirArbol(Recorrido.INFIJO);
+        arbolBin.imprimirArbol(Recorrido.POSFIJO);
+
+        System.out.println();
+          System.out.println("\n--Altura del árbol: " + arbolBin.obtenerAltura());
+
+        System.out.println("--Subarbol E ");
+        arbol.imprimirSubArbol(nodoBinE);
+        System.out.println();
+
+        System.out.println("\n--¿El árbol está vacío?");
+          System.out.println(arbolBin.estaVacio() ? "Sí\n" : "No\n");
+
+        String raizArbolBin = raizBin.getDato();
+        System.out.println("--Raíz del arbol: " + raizArbolBin);
+
+        arbolBin.imprimirSubArbol(nodoBinJ);
   }
   
+}
