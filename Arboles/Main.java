41a7e0fd8a79e78aeff548955661425b1c0e609d
Luis Fernando Reyes Betancourt <luisfrb456@gmail.com>
2023-11-13 20:42:48 -0600

    Cambios comentados

--- Arboles/Main.java
+++ Arboles/Main.java
@@ -1,7 +1,24 @@
+/**
+ * Clase main que contiene la creacion de todos los nodos usados tanto en el arbol normal
+ * como en el arbol binario, la estructura de enlazamiento de todos los nodos hermanos e hijos
+ * en cada tipo de arbol y su impresion con cada tipo de recorrido, asi como la obtencion de datos
+ * como la altura, subarbol, nodo raiz y la comprobacion de estado vacio
+ */
 public class Main {
+
   public static void main(String[] args) {
+
+    /**
+    * Creamos una variable arbol tipo string y la declaramos false
+    * y el nodo A sería nuestra raiz
+     */
     Arbol<String> arbol = new Arbol<>("A", false);
   
+    /**
+     * Creamos todos los nodos que vamos a utilizar
+     * para el arbol normal a partir del nodo B 
+     * hasta el Q
+     */
     NodoArbol<String> nodoRaiz = arbol.obtenerRaiz();
     NodoArbol<String> nodoB = new NodoArbol<>("B");
     NodoArbol<String> nodoC = new NodoArbol<>("C");
@@ -20,55 +37,75 @@ public class Main {
     NodoArbol<String> nodoP = new NodoArbol<>("P");
     NodoArbol<String> nodoQ = new NodoArbol<>("Q");
 
-    // Nodo A y su descendencia
+    /**
+     * Creamos ahora la reación entre los nodos, declrando los
+     * parentescos de hijos, hermanos y nietos de cada nodo
+     */
     arbol.agregarNodoArbol(nodoRaiz, nodoB);
     arbol.agregarNodoArbol(nodoRaiz, nodoC);
     arbol.agregarNodoArbol(nodoRaiz, nodoD);
-    // Nodo B y su descendencia
     arbol.agregarNodoArbol(nodoB, nodoE);
     arbol.agregarNodoArbol(nodoB, nodoF);
-    // Nodo F y su descendencia
     arbol.agregarNodoArbol(nodoF, nodoJ);
     arbol.agregarNodoArbol(nodoF, nodoK);
     arbol.agregarNodoArbol(nodoF, nodoL);
-    // Nodo D y su descendencia
     arbol.agregarNodoArbol(nodoD, nodoG);
     arbol.agregarNodoArbol(nodoD, nodoH);
     arbol.agregarNodoArbol(nodoD, nodoI);
-    // Nodo G y su descendencia
     arbol.agregarNodoArbol(nodoG, nodoM);
-    // Nodo I y su descendencia
     arbol.agregarNodoArbol(nodoI, nodoN);
     arbol.agregarNodoArbol(nodoI, nodoO);
-    // Nodo M y su descendencia
     arbol.agregarNodoArbol(nodoM, nodoP);
     arbol.agregarNodoArbol(nodoM, nodoQ);
 
   System.out.println("\n--Árbol Ordinario--");
     
+  /**
+   * Imprimir cada tipo de recorrido del arbol normal 
+   */
   System.out.println();
     arbol.imprimirArbol(Recorrido.PREFIJO);
     arbol.imprimirArbol(Recorrido.INFIJO);
     arbol.imprimirArbol(Recorrido.POSFIJO);
 
+    /**
+     * Impresion de la altura por medio del método obtenerAltura
+     */
     System.out.println();
     System.out.println("\n--Altura del árbol: " + arbol.obtenerAltura());
 
-        System.out.println("\n--Subarbol B ");
+    /**
+     * Impresion del subarbol a partir del nodo B
+     */
+    System.out.println("\n--Subarbol B ");
         arbol.imprimirSubArbol(nodoB);
         System.out.println();
 
+        /**
+         *  Impresion que verifica si el arbol esta vacio o no
+         */
         System.out.println("\n--¿El árbol está vacío?");
           System.out.println(arbol.estaVacio() ? "Sí\n" : "No\n");
 
-        String raiz = nodoRaiz.getDato();
+        /* 
+        * obtencion del nodo raiz del arbol 
+        */
+          String raiz = nodoRaiz.getDato();
         System.out.println("--Raíz del árbol: " + raiz);
 
          System.out.println("\n--Árbol Binario--");
 
-        Arbol<String> arbolBin = new Arbol<String>("A", true);
+        /**
+         * Creadion de la variable arbolBin para ahora generar la estrutura
+         * de un arbol binario con su nodo A raiz decladaro true
+         */
+         Arbol<String> arbolBin = new Arbol<String>("A", true);
 
-        NodoArbol<String> raizBin = arbolBin.obtenerRaiz();
+        /**
+         * Creación de cada nodo a usar mediante la variable arbolBin con sus respectivos
+         * nodos hijos, hermanos y nietos, desde el B hasta el Q
+         */
+         NodoArbol<String> raizBin = arbolBin.obtenerRaiz();
         NodoArbol<String> nodoBinB = new NodoArbol<>("B");
         NodoArbol<String> nodoBinC = new NodoArbol<>("C");
         NodoArbol<String> nodoBinD = new NodoArbol<>("D");
@@ -86,6 +123,11 @@ public class Main {
         NodoArbol<String> nodoBinP = new NodoArbol<>("P");
         NodoArbol<String> nodoBinQ = new NodoArbol<>("Q");
 
+        /**
+         * Formación de cada nodo hijo a partir de la raiz, teniendo en cuenta
+         * que cada hijo debe tener 2 nodos hijos en este tipo de arbol
+         * organizando asi nuestro arbol binario 
+         */
         arbolBin.agregarNodoArbol(raizBin, nodoBinB);
         arbolBin.agregarNodoArbol(nodoBinB, nodoBinE);
         arbolBin.agregarNodoArbol(nodoBinB, nodoBinC);
@@ -103,22 +145,37 @@ public class Main {
         arbolBin.agregarNodoArbol(nodoBinI, nodoBinN);
         arbolBin.agregarNodoArbol(nodoBinN, nodoBinO);
 
+        /**
+         * impresion ahora de cada recorrido con el arbol binario 
+         */
         System.out.println();
         arbolBin.imprimirArbol(Recorrido.PREFIJO);
         arbolBin.imprimirArbol(Recorrido.INFIJO);
         arbolBin.imprimirArbol(Recorrido.POSFIJO);
 
+        /**
+         * Impresion de obtecion de la altura del arbol 
+         */
         System.out.println();
           System.out.println("\n--Altura del árbol: " + arbolBin.obtenerAltura());
 
-        System.out.println("--Subarbol E ");
+        /**
+         * Impresion del subarbol a partir del nodo E, en este caso
+         */
+          System.out.println("--Subarbol E ");
         arbol.imprimirSubArbol(nodoBinE);
         System.out.println();
 
+        /**
+         * Comprobación del arbol para saber si está vacio o no 
+         */
         System.out.println("\n--¿El árbol está vacío?");
           System.out.println(arbolBin.estaVacio() ? "Sí\n" : "No\n");
 
-        String raizArbolBin = raizBin.getDato();
+        /**
+         * Metodo que nos imprimirá el nodo raiz de este abol binario 
+         */
+          String raizArbolBin = raizBin.getDato();
         System.out.println("--Raíz del arbol: " + raizArbolBin);
 
         arbolBin.imprimirSubArbol(nodoBinJ);
